14d046762cb2b96901823acff2b8535ee00d48d5
Don <donbytyqi@outlook.com>
2019-08-02 11:14:36 +0200

    Added fatalError if API key is missing. Have gotten a few people asking why the app won't load the posts.

--- Papers/helpers/UnsplashAPIService.swift
+++ Papers/helpers/UnsplashAPIService.swift
@@ -10,18 +10,20 @@ import Foundation
 
 struct UnsplashAPIService {
     
-    static let apiKey = "93404f08ee44f12d92029e7af3cb8803d63f52592e9ab463a70d0c77b0b4d159"
+    static let apiKey = ""
     
     static func getPhotos(page: Int, orderBy: String, username: String = "", completion: @escaping ([Photo]) -> ()) {
         
         var photos = [Photo]()
         
+        guard !apiKey.isEmpty else {
+            fatalError("Insert your api key from Unsplash.")
+            return }
+        
         guard let url = URL(string: username == "" ? "https://api.unsplash.com/photos?page=\(page)&client_id=\(UnsplashAPIService.apiKey)&per_page=50&order_by=\(orderBy)" : "https://api.unsplash.com/users/\(username.replacingOccurrences(of: " ", with: ""))/photos?page=\(page)&client_id=\(UnsplashAPIService.apiKey)") else {
             print("URL is invalid.")
             completion(photos)
             return }
-        
-        print("URL", url)
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             
